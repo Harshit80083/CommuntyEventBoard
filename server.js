3db83872c98c0977948ded989772eab468e0a6eb
Harshit Sharma <harshitsharma@Harshits-MacBook-Air-6.local>
2026-04-23 20:01:46 +0530

    fixed mongo uri error

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.static('public'));
 // connect to mongodb
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const uri = process.env.MONGO_URI || "mongodb://localhost:27017/community-events";
+    await mongoose.connect(uri);
     console.log('connected to mongodb');
   } catch (err) {
     console.log('mongodb connection error', err);
@@ -394,7 +395,7 @@ app.delete('/api/admin/users/:id', authMiddleware, adminMiddleware, async (req, 
 });
 
 // start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('server running on http://localhost:' + PORT);
 });
